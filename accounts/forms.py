0e457119cbf513e98ddb717d58777bca6fd6c489
jaejae200 <kj31049@naver.com>
2022-10-14 11:05:11 +0900

    ADD update

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,14 @@
 from dataclasses import field
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ('username', 'password1', 'password2',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('first_name', 'last_name', 'email',)
